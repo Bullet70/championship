a20da34949c019a35438c31e947b6c52c6dc708a
Specia_R <esinware_specia@camera.it>
2020-03-11 17:53:21 +0100

    Modification test

--- BackEnd/src/main/java/it/esinware/championship/domain/Squadra.java
+++ BackEnd/src/main/java/it/esinware/championship/domain/Squadra.java
@@ -29,12 +29,13 @@ public class Squadra implements Comparable<Squadra> {
 	}
 	
 	// ordinamento oggetti di tipo squadra
-	if(nome.equals(s.getNome))return 0;
-	int risultato = punti - s.getPunti();
+//	if(nome.equals(s.getNome))return 0;
+//	int risultato = punti - s.getPunti();
 
 	@Override
 	public int compareTo(Squadra o) {
 		// TODO Auto-generated method stub
 		return 0;
-	}if(risultato==0)risultato=nome.compareTo(s.getNome());return risultato;
-}} // classe
+	}
+//	if(risultato==0)risultato=nome.compareTo(s.getNome());return risultato;
+}// classe
