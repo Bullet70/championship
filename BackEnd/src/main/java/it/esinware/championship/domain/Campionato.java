b5aa9b45c266cb72090e7309866f07e95080fc6a
Riccardo Tamanti <riccardo10.tamanti@gmail.com>
2020-03-13 17:31:14 +0100

    Campionato.java Giornata.java

---
+++ BackEnd/src/main/java/it/esinware/championship/domain/Campionato.java
@@ -0,0 +1,30 @@
+package it.esinware.championship.domain;
+
+public class Campionato {
+	private String nameChampionship;
+	private int startYear;
+	private int endYear;
+	
+	public String getNameChampionship() {
+		return nameChampionship;
+	}
+	public void setNameChampionship(String nameChampionship) {
+		this.nameChampionship = nameChampionship;
+	}
+	public int getStartYear() {
+		return startYear;
+	}
+	public void setStartYear(int startYear) {
+		this.startYear = startYear;
+	}
+	public int getEndYear() {
+		return endYear;
+	}
+	public void setEndYear(int endYear) {
+		this.endYear = endYear;
+	}
+	
+	
+	
+
+}
---
+++ BackEnd/src/main/java/it/esinware/championship/domain/Giornata.java
@@ -0,0 +1,35 @@
+package it.esinware.championship.domain;
+
+public class Giornata {
+	private int date;
+	private String match;
+	private int numberRound;
+	//get/set
+	public int getDate() {
+		return date;
+	}
+	
+	public void setDate(int date) {
+		this.date = date;
+	}
+
+	public String getMatch() {
+		return match;
+	}
+
+	public void setMatch(String match) {
+		this.match = match;
+	}
+
+	public int getNumberRound() {
+		return numberRound;
+	}
+
+	public void setNumberRound(int numberRound) {
+		this.numberRound = numberRound;
+	}
+	 
+	
+	
+
+}
