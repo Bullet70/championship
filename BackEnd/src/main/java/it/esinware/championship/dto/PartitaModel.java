b918223cad38942dbb5c374af2fbd55fb25d13ef
Specia_R <esinware_specia@camera.it>
2020-04-07 14:21:45 +0200

    View now

--- BackEnd/src/main/java/it/esinware/championship/dto/PartitaModel.java
+++ BackEnd/src/main/java/it/esinware/championship/dto/PartitaModel.java
@@ -11,15 +11,9 @@ public class PartitaModel implements Serializable{
 	private static final long serialVersionUID = 4364972170687145136L;
 	
 	private String description;
-	private SortedSet<PartitaModel> teams;
+	private SquadraModel homeTeam;
+	private SquadraModel guestTeam;
 	private SortedSet<PartitaModel>scores;
-	
-	public void addTeam(PartitaModel team) {
-		if(teams == null)
-			teams = new TreeSet<>();
-		if(!teams.contains(team))
-			teams.add(team);
-	}
 	
 	public void addScore(PartitaModel score) {
 		if(scores == null)
