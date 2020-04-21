a5e418cfd2511426e95ad10556f959eeefa415b7
Riccardo Tamanti <riccardo10.tamanti@gmail.com>
2020-04-21 15:57:26 +0200

    .

--- BackEnd/src/ui/app/components/menu.components.ts
+++ BackEnd/src/ui/app/components/menu.components.ts
@@ -9,38 +9,21 @@ import { MenuItem } from 'primeng/api';
 })
 export class MenuComponent {
 	
-	items:MenuItem[];
+	items1:MenuItem[];
 	
 	
 	activeItem:MenuItem;
 	
 
     ngOnInit() {
-        this.items = [
+        this.items1 = [
             {label: 'Italia', icon: 'pi pi-fw pi-squadre', routerLink: 'championships'},
             {label: 'Germania', icon: 'pi pi-fw pi-calendario', routerLink: 'championships'},
             {label: 'Inghilterra', icon: 'pi pi-fw pi-classifica', routerLink: 'championships'},
             {label: 'Spagna', icon: 'pi pi-fw pi-marcatori', routerLink: 'championships'},
         ];
 
-		 this.activeItem = this.items[0];
-
-		 
-	
-
-        this.items = [
-            {label: 'Serie A', icon: 'pi pi-fw pi-seriea'},
-            {label: 'Serie B', icon: 'pi pi-fw pi-serieb'},
-            {label: 'Serie C', icon: 'pi pi-fw pi-seriec'},
-           
-        ];
-
-		 this.activeItem = this.items[1];
-
-
-
-
 		
-    }
+	}
 }
 
