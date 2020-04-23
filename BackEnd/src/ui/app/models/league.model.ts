46c05a9baef42cfa460d678b5dfcbe13e453e2be
Riccardo Tamanti <riccardo10.tamanti@gmail.com>
2020-04-23 15:00:08 +0200

    aggiunta campionati

--- BackEnd/src/ui/app/models/league.model.ts
+++ BackEnd/src/ui/app/models/league.model.ts
@@ -9,7 +9,19 @@ export const LEAGUES: LeagueModel[] = [
   	{id: 2, name: 'Serie B', country: 1},
   	{id: 3, name: 'Lega Pro', country: 1},
   	{id: 4, name: 'Bundesliga', country: 2},
-  	{id: 5, name: 'Premier League', country: 3},
-  	{id: 6, name: 'La Liga', country: 4},
-  	{id: 7, name: 'Ligue 1', country: 5}
+	{id: 5, name: 'Zweite Bundesliga', country: 2},
+	{id: 6, name: '3.Liga', country: 2},
+  	{id: 7, name: 'Premier League', country: 3},
+	{id: 8, name: 'Championship', country: 3},
+	{id: 9, name: 'League One', country: 3},  
+  	{id: 10, name: 'La Liga', country: 4},
+	{id: 11, name: 'Segunda Division', country: 4},
+	{id: 12, name: 'Segunda Division B', country: 4},
+  	{id: 13, name: 'Ligue 1', country: 5},
+	{id: 14, name: 'Ligue 2', country: 5},
+	{id: 15, name: 'Championnat National', country: 5},
+	{id: 16, name: 'Eredivise', country: 6},
+	{id: 17, name: 'Eerste Divisie', country: 6},
+	{id: 18, name: 'Derde Divisie', country: 6}
+	
 ]
